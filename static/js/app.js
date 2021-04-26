5ebce1c9109b9de0e5c9bd14428a6913fa562166
mpetersen49 <mpetersen49@gmail.com>
2021-04-26 18:00:23 -0500

    Added circles to chart and axis labels

--- static/js/app.js
+++ static/js/app.js
@@ -5,10 +5,10 @@ var svgWidth = 960;
 var svgHeight = 500;
 
 var margin = {
-  top: 20,
-  right: 40,
-  bottom: 80,
-  left: 100
+    top: 20,
+    right: 40,
+    bottom: 60,
+    left: 100
 };
 
 var width = svgWidth - margin.left - margin.right;
@@ -24,7 +24,7 @@ var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 d3.csv("static/data/data.csv").then(data => {
-    console.log(data);
+    //console.log(data);
 
     data.forEach(obj => {
         obj.poverty = +obj.poverty;
@@ -34,7 +34,7 @@ d3.csv("static/data/data.csv").then(data => {
     // Step 2: Create scale functions
     // ==============================
     var xLinearScale = d3.scaleLinear()
-      .domain([20, d3.max(data, d => d.poverty)])
+      .domain([8, d3.max(data, d => d.poverty)])
       .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
@@ -55,4 +55,32 @@ d3.csv("static/data/data.csv").then(data => {
     chartGroup.append("g")
       .call(leftAxis);
 
+    // Step 5: Create Circles
+    // ==============================
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", "15")
+    .attr("fill", "lightblue")
+    .attr("opacity", ".5");
+
+    // Create axes labels
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 30)
+      .attr("x", 0 - (height / 1.4))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Number of Billboard 100 Hits"); //change title
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("class", "axisText")
+      .text("Hair Metal Band Hair Length (inches)"); //change title
+
+}).catch(function(error) {
+    console.log(error);
 });
