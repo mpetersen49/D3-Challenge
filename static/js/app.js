a55052b9f637c8147dc0e3bac80f5e80473f5ec4
mpetersen49 <mpetersen49@gmail.com>
2021-04-30 16:59:04 -0500

    Added state abbr labels to cirles

--- static/js/app.js
+++ static/js/app.js
@@ -58,28 +58,59 @@ d3.csv("static/data/data.csv").then(data => {
     // Step 5: Create Circles
     // ==============================
     var circlesGroup = chartGroup.selectAll("circle")
-    .data(data)
-    .enter()
-    .append("circle")
-    .attr("cx", d => xLinearScale(d.poverty))
-    .attr("cy", d => yLinearScale(d.healthcare))
-    .attr("r", "15")
-    .attr("fill", "lightblue")
-    .attr("opacity", ".5");
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("cx", d => xLinearScale(d.poverty))
+      .attr("cy", d => yLinearScale(d.healthcare))
+      .attr("r", "15")
+      .attr("fill", "lightblue")
+      .attr("opacity", ".5");
+
+    var abbrGroup = chartGroup.selectAll(null)
+      .data(data)
+      .enter()
+      .append("text")
+      .text(d => d.abbr)
+      .attr("x", d => xLinearScale(d.poverty))
+      .attr("y", d => yLinearScale(d.healthcare))
+      .attr("font-size", 10);
+
+    // Step 6: Initialize tool tip
+    // ==============================
+    var toolTip = d3.tip()
+      .attr("class", "tooltip")
+      .offset([80, -60]) //might adjust this*****************************
+      .html(function(d) {
+        return (`${d.state}<br>Poverty: ${d.poverty}%<br>Heathcare: ${d.healthcare}%`);
+      });
+
+    // Step 7: Create tooltip in the chart
+    // ==============================
+    chartGroup.call(toolTip);
+
+    // Step 8: Create event listeners to display and hide the tooltip
+    // ==============================
+    circlesGroup.on("click", function(data) {
+      toolTip.show(data, this);
+    })
+      .on("mouseout", function(data, index) {
+        toolTip.hide(data)
+      });
 
     // Create axes labels
     chartGroup.append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 0 - margin.left + 30)
-      .attr("x", 0 - (height / 1.4))
+      .attr("x", 0 - (height / 2))
       .attr("dy", "1em")
       .attr("class", "axisText")
-      .text("Number of Billboard 100 Hits"); //change title
+      .text("Lacks Healthcare (%)"); //change title
 
     chartGroup.append("text")
       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
       .attr("class", "axisText")
-      .text("Hair Metal Band Hair Length (inches)"); //change title
+      .text("In Poverty (%)"); //change title
 
 }).catch(function(error) {
     console.log(error);
